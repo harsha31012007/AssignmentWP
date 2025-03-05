42cc5a09ecb7536ce160d0799d25924dcfabce43
harsha31012007 <kh24csb0b36@student.nitw.ac.in>
2025-03-05 11:37:00 +0530

    Add files via upload

---
+++ a8/class8.js
@@ -0,0 +1,25 @@
+let card=document.querySelector('.s');
+let btns=document.querySelectorAll('.item button');
+let span=document.querySelector('#y');
+for(let i=0;i<btns.length;i++){
+    btns[i].addEventListener('click',function(){
+        let item=this.parentElement.children;
+        let div=document.createElement('div');
+        let img=document.createElement('img');
+        let pa=item[0].getAttribute('src');
+        img.setAttribute('src',pa);
+        let h3=document.createElement('h3');
+        let p=document.createElement('p');
+        h3.innerText=item[1].innerText;
+        p.innerText=item[2].innerText;
+        let a=item[2].children[0];
+        let b=Number(span.innerText);
+        let c=Number(a.innerText);
+        span.innerText=b+c;
+        div.append(img);
+        div.append(h3);
+        div.append(p);
+        card.append(div);
+        div.classList.add('item1');
+});
+}
